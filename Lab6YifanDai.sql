9e1725716aba90b9dcc37fed9faff434de81931b
Yifan(Iris) Dai <99828528+yifand9800@users.noreply.github.com>
2022-03-09 15:56:48 -0800

    Update Lab6YifanDai.sql

--- Lab6YifanDai.sql
+++ Lab6YifanDai.sql
@@ -1,17 +1,21 @@
-/* Question 2*/
+/* This assignment is to write MySQL commands to retrieve, add, and update information in the world Database, which is a built-in database of the MySQL program on the Foster Remote 
+Lab. It has three tables: country, city, and countrylanguage. They include various information about over 200 countries and more than 4000 cities in the world 
+such as each country’s name, population, GDP, capital, etc. */
+
+/*Question 2: show all the records in the countrylanguage table.*/
 SELECT *
 FROM countrylanguage;
 
-/* Question 3*/
+/* Question 3: count the number of records in the countrylanguage table. */
 SELECT count(*) AS totalRecords
 FROM countrylanguage;
 
-/* Question 4*/
+/* Question 4: List the CountryCode and Language columns for all the records in the countrylanguage table that are listed as the official language of the country. */
 SELECT CountryCode, Language
 FROM countrylanguage
 WHERE IsOfficial = "T";
 
-/* Question 5*/
+/* Question 5: count the number of languages each country has in the countrylanguage table and list the results by country in an alphabetical order. */
 SELECT CountryCode, count(Language) AS Count
 FROM countrylanguage
 GROUP BY CountryCode
